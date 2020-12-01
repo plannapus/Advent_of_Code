e89765b2bc5f6a16710ca790093b8c4f83071c1d
Johan.Renaudie <johan.renaudie@mfn-berlin.de>
2020-12-01 09:00:04 +0100

    day1

---
+++ 2020/aoc01.R
@@ -0,0 +1,16 @@
+# Part 1
+input <- scan("input01.txt")
+li <- do.call(rbind,lapply(input,function(x)if(any((x+input)==2020))c(which(input==x),which((x+input)==2020))))
+prod(input[li[1,]])
+
+# Part2
+n <- seq_along(input)
+for(i in n){
+  for(j in n[-i]){
+    all <- input[i]+input[j]+input
+    k <- which(all==2020)
+    if(length(k)){
+      stop(input[i]*input[j]*input[k])
+    }
+  }
+}
