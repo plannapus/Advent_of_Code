3f7592ed259ba3a1cc8908fb3ef462b38f246a57
johan.renaudie <johan.renaudie@mfn-berlin.de>
2020-12-19 09:04:40 +0100

    day19 part1

---
+++ 2020/aoc19_part1.R
@@ -0,0 +1,45 @@
+input <- readLines("input19.txt")
+w<-which(input=="")
+messages <- input[(w+1):length(input)]
+rules <- input[1:(w-1)]
+rules <- as.data.frame(do.call(rbind,strsplit(rules,": ")))
+rules[,2]<-paste0("(",gsub("\\\"","",rules[,2]),")")
+rules[,1]<-as.integer(rules[,1])
+rules <- rules[order(rules[,1]),]
+#mod <- as.list(rules[,2])
+W <- c()
+while(grepl("[0-9]",rules[1,2])){
+  w <- grep("^[^0-9]+$",rules[,2])
+  w <- w[!w%in%W]
+  for(i in seq_along(w)){
+    ind <- rules[w[i],1]
+    for(j in grep(sprintf("\\b%s\\b",ind),rules[,2])){
+      rules[j,2] <- gsub(sprintf("\\b%s\\b",ind),rules[w[i],2],rules[j,2])
+    }
+  }
+  W <- c(W,w) 
+}
+sum(grepl(sprintf("^%s$",gsub(" ","",rules[1,2])),messages))
+#151
+
+w<-which(input=="")
+rules <- input[1:(w-1)]
+rules <- as.data.frame(do.call(rbind,strsplit(rules,": ")))
+rules[,2]<-paste0("(",gsub("\\\"","",rules[,2]),")")
+rules[,1]<-as.integer(rules[,1])
+rules <- rules[order(rules[,1]),]
+rules[9,2] <- "42 | 42 (?R)"
+rules[12,2] <- "42 31 | 42 (?R) 31"
+W <- c()
+while(grepl("[0-9]",rules[1,2])){
+  w <- grep("^[^0-9]+$",rules[,2])
+  w <- w[!w%in%W]
+  for(i in seq_along(w)){
+    ind <- rules[w[i],1]
+    for(j in grep(sprintf("\\b%s\\b",ind),rules[,2])){
+      rules[j,2] <- gsub(sprintf("\\b%s\\b",ind),rules[w[i],2],rules[j,2])
+    }
+  }
+  W <- c(W,w) 
+}
+sum(grepl(sprintf("^%s$",gsub(" ","",rules[1,2]),perl=TRUE),messages))
