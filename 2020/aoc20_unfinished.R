6e516a0ae371bedce901cc007ddbc114eaaca88b
johan.renaudie <johan.renaudie@mfn-berlin.de>
2020-12-20 13:42:48 +0100

    day 20 part 1

--- 2020/aoc20_unfinished.R
+++ 2020/aoc20_unfinished.R
@@ -11,9 +11,9 @@ for(i in 1:144){
 }
 
 sides <- lapply(tiles,function(x)c(paste(x$tile[1,],collapse=""),
-                          paste(x$tile[10,],collapse=""),
-                          paste(x$tile[,1],collapse=""),
-                          paste(x$tile[,10],collapse="")))
+                                   paste(x$tile[,10],collapse=""),
+                                   paste(x$tile[10,],collapse=""),
+                                   paste(x$tile[,1],collapse="")))
 n_matching_sides <- c()
 for(i in 1:144){
   n_matching_sides[i]<-sum(sapply(sides[[i]],function(x)x%in%unlist(sides[-i])|intToUtf8(rev(utf8ToInt(x)))%in%unlist(sides[-i])))
@@ -27,62 +27,127 @@ for(i in 1:144){
   tiles[[i]]$matching_sides <- sapply(sides[[i]],function(x)x%in%unlist(sides[-i])|intToUtf8(rev(utf8ToInt(x)))%in%unlist(sides[-i]))
   tiles[[i]]$n_matching <- n_matching_sides[i]
 }
+for(i in 1:144){
+  y <- tiles[[i]]$sides
+  mat <- lapply(tiles[-i],function(X){
+    m<-sapply(X$sides,function(x)x==y|intToUtf8(rev(utf8ToInt(x)))==y)
+    if(any(m)){
+      return(cbind(X$nb,which(m,arr.ind=T)))
+      }else{return(NULL)}
+    })
+  tiles[[i]]$matched <- do.call(rbind,mat)
+}
 
-orient <- function(next_tile,w,side="right"){
-  if(side=="right"){
-    if(w[1]==1) next_tile <- t(next_tile[10:1,])
-    if(w[1]==2) next_tile <- t(next_tile[,10:1])
-    if(w[1]==4) next_tile <- next_tile[,10:1]
-    if(w[1]==5) next_tile <- t(next_tile)
-    if(w[1]==6) next_tile <- t(next_tile[10:1,10:1])
-    if(w[1]==7) next_tile <- next_tile[10:1,]
-    if(w[1]==8) next_tile <- next_tile[10:1,10:1]
-  }else if(side=="bottom"){
-    if(w[1]==4) next_tile <- t(next_tile[10:1,])
-    if(w[1]==7) next_tile <- t(next_tile[,10:1])
-    if(w[1]==5) next_tile <- next_tile[,10:1]
-    if(w[1]==3) next_tile <- t(next_tile)
-    if(w[1]==8) next_tile <- t(next_tile[10:1,10:1])
-    if(w[1]==2) next_tile <- next_tile[10:1,]
-    if(w[1]==6) next_tile <- next_tile[10:1,10:1]
+reorient <- function(tile,side,previous,from="left"){
+  if(from=="left"){
+    w <- which(c(tile$side[side]==previous$side[2],intToUtf8(rev(utf8ToInt(tile$side[side])))==previous$side[2]))
+    if((side==4&from=="left")|(side==1&from=="top")){
+      if(w==1){
+        ord <- order(1:4)
+      }else{
+        tile$tile <- tile$tile[10:1,]
+        ord <- order(c(3,2,1,4))
+      }
+    }
+    if(side==2){
+      if(w==1){
+        tile$tile <- tile$tile[,10:1]
+        ord <- order(c(1,4,3,2))
+      }else{
+        tile$tile <- tile$tile[10:1,10:1]
+        ord <- order(c(3,4,1,2))
+      }
+    }
+    if(side==1){
+      if(w==1){
+        tile$tile <- t(tile$tile)
+        ord <- order(c(2,3,4,1))
+      }else{
+        tile$tile <- t(tile$tile)[10:1,]
+        ord <- order(c(4,3,2,1))
+      }
+    }
+    if(side==3){
+      if(w==1){
+        tile$tile <- t(tile$tile)[,10:1]
+        ord <- order(c(2,1,4,3))
+      }else{
+        tile$tile <- t(tile$tile)[10:1,10:1]
+        ord <- order(c(4,1,2,3))
+      }
+    }
   }
-  next_tile
+  if(from=="top"){
+    w <- which(c(tile$side[side]==previous$side[3],intToUtf8(rev(utf8ToInt(tile$side[side])))==previous$side[3]))
+    if(side==1){
+      if(w==1){
+        ord <- 1:4
+      }else{
+        tile$tile <- tile$tile[,10:1]
+        ord <- order(c(1,4,3,2))
+      }
+    }
+    if(side==2){
+      if(w==1){
+        tile$tile <- t(tile$tile[10:1,])
+        ord <- order(c(2,3,4,1))
+      }else{
+        tile$tile <- t(tile$tile[10:1,10:1])
+        ord <- order(c(2,1,4,3))
+      }
+    }
+    if(side==3){
+      if(w==1){
+        tile$tile <- tile$tile[10:1,]
+        ord <- order(c(3,2,1,4))
+      }else{
+        tile$tile <- tile$tile[10:1,10:1]
+        ord <- order(c(3,4,1,2))
+      }
+    }
+    if(side==4){
+      if(w==1){
+        tile$tile <- t(tile$tile)
+        ord <- order(c(4,3,2,1))
+      }else{
+        tile$tile <- t(tile$tile)[,10:1]
+        ord <- order(c(4,1,2,3))
+      }
+    }
+  }
+  tile$sides <- tile$sides[ord]
+  tile$matched[,2] <- ord[tile$matched[,2]]
+  tile
 }
 
 map <- matrix(nrow=120,ncol=120)
 
-# first_tile <- tiles[which(n_matching_sides==2)][[1]]$tile
-# fs <- sides[which(n_matching_sides==2)][[1]]
-# W <- which(sapply(fs,function(x)x%in%unlist(sides[which(n_matching_sides>2)])|intToUtf8(rev(utf8ToInt(x)))%in%unlist(sides[which(n_matching_sides>2)])))
-# map[1:10,1:10]<-first_tile[10:1,10:1]
-# used <- tiles[which(n_matching_sides==2)][[1]]$nb
-# for(i in 1:10){
-#   sidetile <- tiles[n_matching_sides==3&!sapply(tiles,function(x)x$nb)%in%used]
-#   s <- sides[n_matching_sides==3&!sapply(tiles,function(x)x$nb)%in%used]
-#   w <- which(sapply(s,function(x)c(x,sapply(x,function(X)intToUtf8(rev(utf8ToInt(X)))))%in%paste(map[1:10,i*10],collapse="")),arr.ind=T)
-#   if(length(w)>2)stop()
-#   next_tile <- sidetile[[w[2]]]$tile
-#   used <- c(used,sidetile[[w[2]]]$nb)
-#   map[1:10,(i*10)+1:10] <- orient(next_tile,w,"right")
-# }
-# for(j in 1:11){
-#   sidetile <- tiles[which(n_matching_sides<4)]
-#   s <- sides[which(n_matching_sides<4)]
-#   w <- which(sapply(s,function(x)c(x,sapply(x,function(X)intToUtf8(rev(utf8ToInt(X)))))%in%paste(map[j*10,1:10],collapse="")),arr.ind=T)
-#   next_tile <- sidetile[[w[2]]]$tile
-#   used <- c(used,sidetile[[w[2]]]$nb)
-#   map[(j*10)+1:10,1:10] <- orient(next_tile,w,"bottom")
-# }
-# for(i in 1:11){
-#   for(j in 1:11){
-#     w <- which(sapply(sides,function(x)c(x,sapply(x,function(X)intToUtf8(rev(utf8ToInt(X)))))%in%paste(map[(j*10)+1:10,i*10],collapse="")),arr.ind=T)
-#     next_tile <- tiles[[w[2]]]$tile
-#     used <- c(used,tiles[[w[2]]]$nb)
-#     map[(j*10)+1:10,(i*10)+1:10] <- orient(next_tile,w,"right")
-#   }
-# }
-# cutmap <- map[-c((0:11)*10+1,(1:12)*10),-c((0:11)*10+1,(1:12)*10)]
+nbs <- sapply(tiles,function(x)x$nb)
+ind <- tiles[which(n_matching_sides==2)][[1]]$nb
+first_tile <- tiles[nbs==ind][[1]]
+first_tile$tile <- first_tile$tile[10:1,10:1]
+first_tile$sides <- first_tile$sides[order(c(3,4,1,2))]
+first_tile$matched[,2] <- order(c(3,4,1,2))[first_tile$matched[,2]]
+map[1:10,1:10]<-first_tile$tile
+used <- ind
+lasttile <-first_tile
+for(i in 1:11){ #First row
+  m <- lasttile$matched
+  M <- m[m[,2]==2,,drop=FALSE]
+  M <- M[!M[,1]%in%used,]
+  if(length(M)>3)stop()
+  nexttile <- tiles[nbs==M[1]][[1]]
+  cat(nexttile$n_matching,"\n")
+  nexttile <- reorient(nexttile,M[3],lasttile,"left")
+  map[1:10,i*10+1:10]<-nexttile$tile
+  used <- c(used,nexttile$nb)
+  lasttile <- nexttile
+}
 
+
+#[...]
+
+cutmap <- map[-c((0:11)*10+1,(1:12)*10),-c((0:11)*10+1,(1:12)*10)]
 monster <- "                  # \n#    ##    ##    ###\n #  #  #  #  #  #   "
 monster <- do.call(rbind,strsplit(el(strsplit(monster,"\n")),""))
 pattern <- which(monster=="#",arr.ind=TRUE)
