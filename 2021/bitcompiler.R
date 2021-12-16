85994ab5a7fbf2391df52d9d5323ec3f7b4f2115
Johan.Renaudie <johan.renaudie@mfn-berlin.de>
2021-12-16 10:44:55 +0100

    if ever reuse, intcode-style

--- 2021/bitcompiler.R
+++ 2021/bitcompiler.R
@@ -1,5 +1,3 @@
-input <- readLines("input16.txt")
-
 parse_input<-function(input)as.integer(el(strsplit(
   paste(sapply(el(strsplit(input,"")),
                function(x)switch(x,
@@ -22,52 +20,7 @@ parse_input<-function(input)as.integer(el(strsplit(
 
 bit2int <- function(x)sum(x*2^(length(x):1-1))
 
-f <- function(binary){
-  version <- bit2int(binary[1:3])
-  V <<- V+version
-  binary <- binary[-(1:3)]
-  type <- bit2int(binary[1:3])
-  binary <- binary[-(1:3)]
-  if(type==4){
-    nb <- c()
-    repeat{
-      n <- binary[1:5]
-      nb <- c(nb,n[2:5])
-      binary <- binary[-(1:5)]
-      if(n[1]==0) break
-    }
-    nb <- bit2int(nb)
-  }else{
-    ltype <- binary[1]
-    binary <- binary[-1]
-    if(ltype==0){
-      l <- binary[1:15]
-      binary <- binary[-(1:15)]
-      l <- bit2int(l)
-      subpack <- binary[1:l]
-      while(length(subpack)){
-        subpack <- f(subpack)$data
-      }
-      binary <- binary[-(1:l)]
-    }else{
-      l <- binary[1:11]
-      binary <- binary[-(1:11)]
-      nsub <- bit2int(l)
-      for(i in 1:nsub){
-        binary <- f(binary)$data
-      }
-    }
-  }
-  list(data=binary, version=version, V=V)
-}
-
-b <- parse_input(input)
-V <- 0
-f(b)$V
-#999
-
-options(digits=22)
-g <- function(binary,verbose=FALSE){
+bitcompiler <- function(binary,verbose=FALSE){
   version <- bit2int(binary[1:3])
   binary <- binary[-(1:3)]
   type <- bit2int(binary[1:3])
@@ -91,7 +44,7 @@ g <- function(binary,verbose=FALSE){
       l <- bit2int(l)
       subpack <- binary[1:l]
       while(length(subpack)){
-        subp <- g(subpack)
+        subp <- bitcompiler(subpack)
         subpack <- subp$data
         subvalue <- c(subvalue,subp$value)
       }
@@ -101,27 +54,19 @@ g <- function(binary,verbose=FALSE){
       binary <- binary[-(1:11)]
       nsub <- bit2int(l)
       for(i in 1:nsub){
-        subp <- g(binary)
+        subp <- bitcompiler(binary)
         binary <- subp$data
         subvalue <- c(subvalue, subp$value)
       }
     }
-    if(type==0){
-      nb <- sum(subvalue)
-    }else if(type==1){
-      nb <- prod(subvalue)
-    }else if(type==2){
-      nb <- min(subvalue)
-    }else if(type==3){
-      nb <- max(subvalue)
-    }else if(type==5){
-      nb <- as.integer(subvalue[1]>subvalue[2])
-    }else if(type==6){
-      nb <- as.integer(subvalue[1]<subvalue[2])
-    }else if(type==7){
-      nb <- as.integer(subvalue[1]==subvalue[2])
-    }
-    
+    nb <- switch(as.character(type),
+                 '0'=sum(subvalue),
+                 '1'=prod(subvalue),
+                 '2'=min(subvalue),
+                 '3'=max(subvalue),
+                 '5'=as.integer(subvalue[1]>subvalue[2]),
+                 '6'=as.integer(subvalue[1]<subvalue[2]),
+                 '7'=as.integer(subvalue[1]==subvalue[2]))
   }
   if(verbose){
     if(type!=4) cat(nb,"\tT=",type,"\tS=",subvalue,"\n")
@@ -129,5 +74,3 @@ g <- function(binary,verbose=FALSE){
   }
   list(data=binary, value=nb)
 }
-g(b)$value
-#3408662834145
