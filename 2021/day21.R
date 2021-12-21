5e8a2c44936cb36f11330fe82f6a4b8be90fa3e2
johan.renaudie <johan.renaudie@mfn-berlin.de>
2021-12-21 11:17:19 +0100

    day 21

--- 2021/day21.R
+++ 2021/day21.R
@@ -12,47 +12,49 @@ while(all(s<1000)){
   d[d>100] <- d[d>100]-100
 }
 n*s[s<1000]
-#989352
-library(bit64)
-p <- c(5,9)
-s <- c(0,0)
+# #989352
+
+#WIP
+options(digits=22)
+M <- array(0,dim=c(10,10,21,21))
+M[5,9,1,1]<-1
+W <- c(0,0)
 dirac <- apply(expand.grid(1:3,1:3,1:3),1,sum)
-td <- integer64()
-td[1:9] <- c(0,0,table(dirac))
-turn <- function(p,s,d,i,dm){
-  i <- as.integer(i)
-  p[i] <- (p[i]+d)%%10
-  if(p[i]==0) p[i] <- 10
-  s[i] <- s[i] + p[i]
-  w <- 0
-  if(s[i]>=21) w <- i
-  dm <- dm*td[d]
-  i <- ifelse(i==1,2,1)
-  res <- integer64(7)
-  res[1:6]<-c(p,s,i,w)
-  res[7] <- dm
-  res
-}
-i=1
-dm=as.integer64(1)
-current <- do.call(rbind,lapply(3:9,function(x)turn(p,s,x,i,dm)))
-W <- integer64(2)
-repeat{
-  nextt <- list()
-  for(j in 1:nrow(current)){
-    if(current[j,6]==0){
-      nextt[[j]]<-do.call(rbind,
-              lapply(3:9,function(x)turn(current[j,1:2],
-                                           current[j,3:4],
-                                           x,
-                                           current[j,5],
-                                           current[j,7])))
-    }else{
-      W[as.integer(current[j,6])] <- W[as.integer(current[j,6])]+current[j,7]
+td <- table(dirac)
+while(sum(M)>0){
+  N <- array(0,dim=c(10,10,21,21))
+  for(i in 1:21){
+    for(j in 1:21){
+      for(k in 1:10){
+        for(l in 1:10){
+          if(M[k,l,i,j]>0){
+            op <- M[k,l,i,j]
+            for(m in 3:9){
+              np1 <- k+m
+              if(np1>10) np1 <- np1-10
+              ns1 <- np1 + i-1
+              if(ns1<21){
+                for(n in 3:9){
+                  np2 <- l+n
+                  if(np2>10) np2 <- np2-10
+                  ns2 <- np2 + j-1
+                  if(ns2<21){
+                    N[np1,np2,ns1+1,ns2+1]<-N[np1,np2,ns1+1,ns2+1]+op*td[m-2]*td[n-2]
+                  }else{
+                    W[2] <- W[2]+op*td[m-2]*td[n-2]
+                  }
+                }
+              }else{
+                W[1] <- W[1]+op*td[m-2]
+              }
+            }
+          }
+        }
+      }
     }
   }
-  current <- do.call(rbind,nextt)
-  cat(nrow(current),":",as.character(W[1]),"-",as.character(W[2]),"\n")
-  if(!nrow(current)) break
+  M<-N
 }
+
 max(W)
+#430229563871565
