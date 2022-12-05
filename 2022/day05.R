8c1ec548acd3961029dbcc87072bbc4682e877a4
johan.renaudie <johan.renaudie@mfn-berlin.de>
2022-12-05 09:56:39 +0100

    added comments on day05

--- 2022/day05.R
+++ 2022/day05.R
@@ -1,14 +1,22 @@
+# Read in file
 input <- readLines("input05.txt")
+# Figure out where the instructions begin
 w <- grep("move",input)[1]
+# Read the crate stacks as a fixed width table
 mat <- read.fwf(file="input05.txt",widths=rep(4,9),n=(w-3))
+# Get rid of non integer characters and clean up
 mat <- apply(mat,2,\(x)gsub("[][ ]","",x))
 stacks <- list()
 for(i in 1:ncol(mat)){
-  stacks[[i]]<-mat[,i]
+  stacks[[i]] <- mat[,i]
   stacks[[i]] <- stacks[[i]][stacks[[i]]!=""]
 }
+# Duplicate the initial setup for part 2
 stacks2 <- stacks
-parse.one <- function(res, result) {
+
+# Parse the instructions
+# Classic function to extract Perl-style named group in regex.
+parse.one <- function(res, result) { #old faithful (from regexpr help file)
   m <- do.call(rbind, lapply(seq_along(res), function(i) {
     if(result[i] == -1) return("")
     st <- attr(result, "capture.start")[i, ]
@@ -19,8 +27,9 @@ parse.one <- function(res, result) {
 }
 parsed <- regexpr("^move (?<n>[0-9]+) from (?<from>[0-9]+) to (?<to>[0-9]+)$", input[w:length(input)], perl=TRUE)
 inst <- parse.one(input[w:length(input)],parsed)
-inst <- apply(inst,2,as.integer)
-inst <- as.data.frame(inst)
+inst <- as.data.frame(apply(inst,2,as.integer))
+
+# Part 1
 for(i in 1:nrow(inst)){
   substack <- stacks[[inst$from[i]]][1:inst$n[i]]
   stacks[[inst$from[i]]] <- stacks[[inst$from[i]]][-(1:inst$n[i])]
@@ -29,6 +38,7 @@ for(i in 1:nrow(inst)){
 cat(sapply(stacks,el),sep="")
 #FRDSQRRCD
 
+# Part 2
 for(i in 1:nrow(inst)){
   substack <- stacks2[[inst$from[i]]][1:inst$n[i]]
   stacks2[[inst$from[i]]] <- stacks2[[inst$from[i]]][-(1:inst$n[i])]
