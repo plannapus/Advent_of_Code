5165bc7202ab0175ff8fcf97a5cac81ab14aeabf
johan.renaudie <johan.renaudie@mfn-berlin.de>
2022-12-19 08:06:24 +0100

    attempts at day 19

--- 2022/day18.R
+++ 2022/day18.R
@@ -25,3 +25,22 @@ for(i in 1:nrow(sides)){
 }
 sum(ext)+5
 #2468
+
+
+###Alt part 2
+allp <- expand.grid(0:22,0:22,0:22)
+is_lava <- apply(allp,1,function(x)anyDuplicated(rbind(x,input)))
+void <- allp[!is_lava,]
+inn <- c()
+for(i in 1:nrow(void)){
+  inn[i]<-all(any(input[,1]%in%-1:void[i,1]&input[,2]==void[i,2]&input[,3]==void[i,3]),
+              any(input[,1]%in%void[i,1]:22&input[,2]==void[i,2]&input[,3]==void[i,3]),
+              any(input[,2]%in%-1:void[i,2]&input[,1]==void[i,1]&input[,3]==void[i,3]),
+              any(input[,2]%in%void[i,2]:22&input[,1]==void[i,1]&input[,3]==void[i,3]),
+              any(input[,3]%in%-1:void[i,3]&input[,1]==void[i,1]&input[,2]==void[i,2]),
+              any(input[,3]%in%void[i,3]:22&input[,1]==void[i,1]&input[,2]==void[i,2]))
+}
+inner <- void[inn,]
+d2 <- dist(inner,"manhattan")
+sum(6-rowSums(as.matrix(d)==1))-sum(6-rowSums(as.matrix(d2)==1))+6
+#2468
