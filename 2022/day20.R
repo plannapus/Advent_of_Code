055ebe0227dc7a5b71a98c0ef5df036126932c64
Johan.Renaudie <johan.renaudie@mfn-berlin.de>
2022-12-20 11:31:44 +0100

    day20

--- 2022/day20.R
+++ 2022/day20.R
@@ -26,27 +26,19 @@ sum(input[(a+c(1000,2000,3000))%%nrow(input),2])
 options(digits=22)
 input <- scan("input20.txt")
 #input <- c(1, 2, -3, 3, -2, 0, 4)
-#input <- cbind(1:length(input),input*811589153)
-input <- cbind(1:length(input),input*4153)
+input <- cbind(1:length(input),input*811589153)
+indices <- 0:(nrow(input)-1)
 for(k in 1:10){
   for(i in seq_len(nrow(input))){
-    w <- which(input[,1]==i)
-    v <- input[w,2]
-    n <- (w+v)
-    while(n<=0){
-      n <- n+nrow(input)-1
-    }
-    while(n>nrow(input)){
-      n <- n-nrow(input)+1
-    }
-    input <- input[-w,]
-    if(n>nrow(input)){
-      input <- rbind(input,c(i,v))
-    }else{
-      input <- rbind(input[seq_len(n-1),],c(i,v),input[n:nrow(input),])
-    }
-    if(!i%%100)cat(k,":",i,"\r")
+    v <- input[input[,1]==i,2]
+    w <- indices[i]
+    n <- (w+v)%%(nrow(input)-1)
+    indices[indices>w] <- (indices[indices>w]-1)%%nrow(input)
+    indices[indices>=n] <- (indices[indices>=n]+1)%%nrow(input)
+    indices[i] <- n
+    #cat(k,":",i,"\r")
   }
 }
-a <- which(input[,2]==0)
-sum(input[(a+c(1000,2000,3000))%%nrow(input),2])
+a <- indices[input[,2]==0]
+sum(input[which(indices%in%((a+c(1000,2000,3000))%%nrow(input))),2])
+#6704537992933
