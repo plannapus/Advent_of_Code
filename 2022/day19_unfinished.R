c1c385c3d281f9ade9e291de35be12031645ff27
Johan.Renaudie <johan.renaudie@mfn-berlin.de>
2022-12-20 14:17:26 +0100

    up

--- 2022/day19_unfinished.R
+++ 2022/day19_unfinished.R
@@ -8,8 +8,8 @@ res <- rep(0,nrow(input))
 
 for(i in 1:nrow(input)){
   cat("\nBlueprint", i,"\n")
-  queue <- data.frame(t=1,
-                      or=1,cl=0,ob=0,ge=0,
+  queue <- data.frame(t=0,
+                      or=0,cl=0,ob=0,ge=0,
                       orbot=1,clbot=0,obbot=0,gebot=0)
   mo <- max(c(input$or_cl[i],input$or_ob[i],input$or_ge[i],input$or_or[i]))
   while(nrow(queue)){
@@ -19,8 +19,10 @@ for(i in 1:nrow(input)){
       res[i] <- pmax(res[i],max(done$ge))
     }else if(any(queue$t==23 & queue$gebot==0)){
       queue <- queue[!(queue$t==23 & queue$gebot==0),,drop=FALSE]
-    }else if(any(apply(queue,1,function(x)x[5]+x[9]*(24-(x[1]+1))+sum(24:(x[1]+1))) < res[i] )){
-      queue <- queue[any(apply(queue,1,function(x)x[5]+x[9]*(24-(x[1]+1))+sum(24:(x[1]+1))) >= res[i] ),,drop=FALSE]
+    }else if(any(queue$t>19 &queue$obbot==0)){#Min obs is 7
+      queue <- queue[!(queue$t>16 & queue$obbot==0),,drop=FALSE]
+    }else if(any(queue$t>14 &queue$clbot==0)){#Min obs is 7
+      queue <- queue[!(queue$t>11 & queue$clbot==0),,drop=FALSE]
     }else{
       state <- queue[1,]
       queue <- queue[-1,,drop=FALSE]
@@ -33,42 +35,38 @@ for(i in 1:nrow(input)){
         state$or <- state$or - input$or_ge[i]
         state$ob <- state$ob - input$ob_ge[i]
         state$gebot <- state$gebot + 1
-        queue <- rbind(state,queue)
+        queue <- rbind(queue,state)
       }else if(state$or>=input$or_ob[i] & state$cl>=input$cl_ob[i] & 
                state$obbot <= input$ob_ge[i]){
         state$or <- state$or - input$or_ob[i]
         state$cl <- state$cl - input$cl_ob[i]
         state$obbot <- state$obbot + 1
-        queue <- rbind(state,queue)
-      }else{
-        if(state$or>=input$or_or[i] & 
-               state$orbot<=input$or_cl[i]){
-        state2 <- state
-        state2$or <- state2$or - input$or_or[i]
-        state2$orbot <- state2$orbot + 1
-        queue <- rbind(queue,state2)
-      }else if(state$or>=input$or_cl[i]& 
-               state$clbot <= input$cl_ob[i]){
+        queue <- rbind(queue,state)
+      }else if(state$or>=input$or_cl[i]|state$or>=input$or_or[i]){
+        if(state$or>=input$or_cl[i]& 
+                state$clbot <= input$cl_ob[i]){
         state3 <- state
         state3$or <- state3$or - input$or_cl[i]
         state3$clbot <- state3$clbot + 1
         queue <- rbind(queue,state3)
-      }else if(state$or>=input$or_or[i] & 
-           state$orbot<=mo){
-          state2 <- state
-          state2$or <- state2$or - input$or_or[i]
-          state2$orbot <- state2$orbot + 1
-          queue <- rbind(queue,state2)
+        }
+        if(state$or>=input$or_or[i] & 
+               state$orbot<=mo){
+        state2 <- state
+        state2$or <- state2$or - input$or_or[i]
+        state2$orbot <- state2$orbot + 1
+        queue <- rbind(queue,state2)
+        }
+        queue <- rbind(state,queue) #Case where nothing is done
       }else{
-        queue <- rbind(queue,state) #Case where nothing is done
-      }
+        queue <- rbind(state,queue) #Case where nothing can be done
       }
       queue <- queue[!duplicated(queue),,drop=FALSE]
       res[i] <- max(c(res[i],queue$ge))
-      if(all(queue$t>=15)) queue <- queue[rev(order(queue$gebot,queue$obbot)),] #Switch to dfs
     }
-    cat(nrow(queue),":",min(queue$t),"-",max(queue$t),":",res[i],"\r")
+    cat(min(queue$t),"-",max(queue$t),":",res[i],"\r")
   }
 }
 cat(sum(res*input$n))
+
 
