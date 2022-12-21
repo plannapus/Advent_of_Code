4d5606eb56a048ef2b2b0356402ff027163c1c68
johan.renaudie <johan.renaudie@mfn-berlin.de>
2022-12-21 14:26:40 +0100

    day21 first part

--- 2022/day21.R
+++ 2022/day21.R
@@ -18,3 +18,27 @@ while(length(bl)){
 }
 root
 #152479825094094
+
+##Not gonna work
+input[grepl("^root",input)]<-gsub("\\+","==",input[grepl("^root",input)])
+input <- input[!grepl("^humn",input)]
+humn <- 0
+root <- FALSE
+while(!root){
+  humn <- humn +1
+  bytype<-split(input, grepl("[0-9]", input))
+  eval(parse(text=bytype[[2]]))
+  bl <- bytype[[1]]
+  while(length(bl)){
+    done<-c()
+    for(i in seq_along(bl)){
+      a<-as.data.frame(parse.group("<- (?<a>[a-z]+) .{1,2} (?<b>[a-z]+)",bl[i]))
+      if(exists(a$a)&exists(a$b)){
+        eval(parse(text=bl[i]))
+        done <- c(done, i)
+      }
+    }
+    bl <- bl[-done]
+  }
+  cat(humn,"\r")
+}
