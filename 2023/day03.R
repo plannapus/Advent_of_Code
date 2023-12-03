59b990e1f16b83fac49eeba4c0eadb546baab0a6
johan.renaudie <johan.renaudie@mfn-berlin.de>
2023-12-03 12:48:13 +0100

    day03

--- 2023/day03.R
+++ 2023/day03.R
@@ -10,7 +10,7 @@ for(i in 1:nrow(input)){
       p <- paste(input[i,j:ncol(input)],collapse="")
       n <- regmatches(p,gregexpr("^[0-9]+",p))[[1]]
       m <- nchar(n)
-      if(any(!input[pmax(i-1,0):pmin(i+1,nrow(input)),pmax((j-1),1):pmin(j+m,ncol(input))]%in%c(".",0:9))){
+      if(any(!input[pmax(i-1,1):pmin(i+1,nrow(input)),pmax(j-1,1):pmin(j+m,ncol(input))]%in%c(".",0:9))){
         N <- c(N,as.integer(n))
         places[[k]] <- cbind(i,j:(j+m-1))
         k <- k+1
@@ -30,8 +30,8 @@ for(g in 1:nrow(gears)){
   n <- 0
   P <- c()
   for(k in seq_along(places)){
-    if(any(places[[k]][,1]%in%pmax(gears[g,1]-1,0):pmin(gears[g,1]+1,nrow(input))&
-           places[[k]][,2]%in%pmax(gears[g,2]-1,0):pmin(gears[g,2]+1,ncol(input)))){
+    if(any(places[[k]][,1]%in%pmax(gears[g,1]-1,1):pmin(gears[g,1]+1,nrow(input))&
+           places[[k]][,2]%in%pmax(gears[g,2]-1,1):pmin(gears[g,2]+1,ncol(input)))){
       n <- n+1
       P <- c(P,N[k])
     }
