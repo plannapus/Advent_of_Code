24840b0ba115b0bb3b185bbc0e29526ac70640e9
johan.renaudie <johan.renaudie@mfn-berlin.de>
2023-12-06 09:29:25 +0100

    day06

--- 2023/day06.R
+++ 2023/day06.R
@@ -1,8 +1,6 @@
 input <- readLines(read.input(6))
-
 time <- as.integer(strsplit(gsub("Time: +","",input[1])," +")[[1]])
 distance <- as.integer(strsplit(gsub("Distance: +","",input[2])," +")[[1]])
-
 poss <- c()
 for(i in seq_along(time)){
   a <- 1:time[i]
@@ -12,8 +10,14 @@ prod(poss)
 #3317888
 
 options(digits=22)
-time <- as.integer(paste(time,collapse=""))
-distance <- as.numeric(paste(distance,collapse=""))
-a <- as.numeric(1:time)
-sum((time-a)*a>distance)
+time2 <- as.integer(paste(time,collapse=""))
+distance2 <- as.numeric(paste(distance,collapse=""))
+a <- as.numeric(1:time2)
+sum((time2-a)*a>distance2)
+#24655068
+
+##Alternative part 2
+time2 <- as.integer(paste(time,collapse=""))
+distance2 <- as.numeric(paste(distance,collapse=""))
+floor((time2+sqrt(time2^2-4*distance2))/2) - ceiling((time2-sqrt(time2^2-4*distance2))/2) + 1
 #24655068
