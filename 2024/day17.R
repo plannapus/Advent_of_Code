2a2c4dd739c65e80ee0dddff6cec54fbb4ca4dbf
johan.renaudie <johan.renaudie@mfn-berlin.de>
2024-12-18 15:00:48 +0100

    finished day17

--- 2024/day17.R
+++ 2024/day17.R
@@ -47,9 +47,57 @@ cat(output,sep=",")
 #7,1,2,3,2,6,7,2,5
 
 options(digits=22)
+
+# Following 3 functions from https://github.com/GjjvdBurg/R-bitops64
+# as base R bitXor can't deal with integer >32 bits
+toBitArr <- function(x) {
+  if (x != 0) {
+    return(c(toBitArr(x %/% 2), c(x %% 2)))
+  } else {
+    return(x)
+  }
+}
+
+fromBitArr <- function(x) {
+  x <- as.integer(x)
+  y <- rev(x)
+  num <- as.integer64(0)
+  i <- 0
+  for (c in y) {
+    num <- num + c * (2^i)
+    i <- i + 1
+  }
+  return(num)
+}
+
+
+bOp <- function(x, y, op = NULL) {
+  require(bit64)
+  x <- as.integer64(x)
+  y <- as.integer64(y)
+  a <- toBitArr(x)
+  b <- toBitArr(y)
+  
+  if (length(a) < length(b)) {
+    t <- b
+    b <- a
+    a <- t
+  }
+  n <- length(a)
+  m <- length(b)
+  if (n != m) {
+    b <- c(c(1:(n - m)) * as.integer64(0), b)
+  }
+  d <- c(1:n) * 0
+  for (i in 1:n) {
+    d[n - i + 1] <- xor(a[n - i + 1], b[n - i + 1])
+  }
+  return(fromBitArr(d))
+}
+
 opcode <- \(prog,A,B,C){
   combo <- \(x){
-    if(x%in%0:3) return(x)
+    if(x%in%0:3) return(as.integer64(x)) #I put as.integer64 everywhere because i m not sure which operator coerce what in what
     if(x==4) return(A)
     if(x==5) return(B)
     if(x==6) return(C)
@@ -58,43 +106,56 @@ opcode <- \(prog,A,B,C){
   output <- c()
   while(i<=length(prog)){
     if(prog[i]==0){
-      A <- floor(A/(2^combo(prog[i+1])))
+      A <- as.integer64(floor(A/(2^combo(prog[i+1]))))
       i <- i+2
     }else if(prog[i]==1){
-      B <- bitXor(B,prog[i+1])
+      B <- bOp(B,prog[i+1])
       i <- i+2
     }else if(prog[i]==2){
       B <- combo(prog[i+1])%%8
       i <- i+2
     }else if(prog[i]==3){
-      if(A==0){
+      if(A==as.integer64(0)){
         i <- i+2
       }else{
         i <- prog[i+1]+1
       }
     }else if(prog[i]==4){
-      B <- bitXor(B,C)
+      B <- bOp(B,C)
       i <- i+2
     }else if(prog[i]==5){
-      output <- c(output,combo(prog[i+1])%%8)
+      output <- c(output,as.character(combo(prog[i+1])%%8))
       i <- i+2
     }else if(prog[i]==6){
-      B <- floor(A/(2^combo(prog[i+1])))
+      B <- as.integer64(floor(A/(2^combo(prog[i+1]))))
       i <- i+2
     }else if(prog[i]==7){
-      C <- floor(A/(2^combo(prog[i+1])))
+      C <- as.integer64(floor(A/(2^combo(prog[i+1]))))
       i <- i+2
     }
   }
   output
-  #return(paste(output,collapse=","))
 }
 
-a <- 0
-repeat{ #damn... bitXor and %% don t work with >32 bits integer apparently...
-  o <- opcode(prog,a,0,0)
-  if(identical(o,prog)) break
-  a <- a+1
-  if(!a%%1000)cat(a,"\r")
+a <- b <- c <- 0
+a <- as.integer64(a)
+b <- as.integer64(b)
+c <- as.integer64(c)
+i <- 16
+
+repeat{
+  if(i==0) break
+  for(j in 0:8){
+    A <- a*as.integer64(8) + as.integer64(j)
+    o <- opcode(prog,A,b,c)
+    if(o[1]==prog[i]){
+      a <- A
+      i <- i-1
+      break
+    }
+    cat(as.character(A),"\r")
+  }
+  cat("a=",as.character(a),"\n")
 }
 a
+#202356708354602
